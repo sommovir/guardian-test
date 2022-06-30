3e145b6119a77ef3416c1abc9e7a96175ad684ae
Luca <luca.coraci@gmail.com>
2022-06-30 12:51:21 +0200

    key exposed in public

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+
+    public static final String version = "1.0";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
